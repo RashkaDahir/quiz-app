d210b5859657da8384ff1114db353581ef7be836
Rashka dahir <rashkajr8899@gmail.com>
2022-12-29 19:42:10 +0300

    done

---
+++ backend-server/database/data.js
@@ -0,0 +1,49 @@
+export default [
+    {
+        id: 1,
+        question : "Sheeg nooca dhiiga AB nooca kale ee dhiig ee uu qaadan karo",
+        options : [
+            'O+',
+            'dhammaan noocyada kale ee dhiigga',
+            'A-',
+        ]
+    },
+    {
+        id: 2,
+        question : "Sheeg sanadkii la aasaasay xiriirka Kubbadda Cagta ee Qaaradda Afrika?",
+        options : [
+            '1999',
+            '1956',
+            '2000',
+        ]
+    },
+    {
+        id: 3,
+        question : "Sheeg Magacyada laba suuradood oo ku soo degay magaalada Maka mid kastana  aayadaheedu yihiin 88 aayadood?",
+        options : [
+            'Al- Baqra iyo Al- Qamar',
+            'Al- Xaj , Arraxmaan',
+            'Al- Qasas , Saad',
+        ]
+    },
+    {
+        id: 4,
+        question : "Sheeg magaca nebigii ugu horreeyey ee qisadiisa lagu xusay quraanka kariimka?",
+        options : [
+            'Nebi ciise (C.S)',
+            'Nebi Aadam (C.S)',
+            'Nebi yuusuf (C.S)'
+        ]
+    },
+    {
+        id: 5,
+        question : "Sheeg magaca dhabta ah ee saxaabigii Abii Muusaa Al- Ashcari (A.R)?",
+        options : [
+            'c/llaahi Bin Qays (A.R)',
+            'Ahmed binu ciise',
+            'Ahmed binu cabdulahi',
+        ]
+    }
+];
+
+export const answers = [1, 1, 2, 1, 0];
